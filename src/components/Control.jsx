c8334e0823d1c320af289251d1feaa27696dd78f
nuastha <timilhllo@gmail.com>
2025-04-09 19:45:33 +0545

    feat: add removeText functionality and enhance text settings layout in SettingsPanel

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -2,8 +2,14 @@ import { useCanvas } from "../context/CanvasContext";
 import Button from "./buttons/Button";
 
 export const SettingsPanel = () => {
-  const { texts, updateText, addNewText, canvasRef, setImageFromFile } =
-    useCanvas();
+  const {
+    texts,
+    updateText,
+    addNewText,
+    canvasRef,
+    setImageFromFile,
+    removeText,
+  } = useCanvas();
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -22,13 +28,15 @@ export const SettingsPanel = () => {
   return (
     <div className="w-1/2 p-4 bg-white shadow-md rounded-lg">
       <div className="w-full space-y-3">
-        <div>
+        <div className="flex gap-2 mb-4 flex-row-reverse items-baseline justify-end">
           <label className="block text-sm font-medium text-gray-700 mb-2">
             Upload Image
           </label>
           <input
             type="file"
-            className="p-8 border border-gray-300 bg-gray-200 rounded "
+            id="imageUpload"
+            name="imageUpload"
+            className="p-4 border border-gray-300 bg-gray-200 rounded "
             accept="image/*"
             onChange={handleImageUpload}
           />
@@ -36,73 +44,97 @@ export const SettingsPanel = () => {
         <h2 className="text-lg font-semibold mb-4">Text Settings</h2>
         <div>
           {texts.map((t) => (
-            <div key={t.id} className="flex gap-2 items-start flex-col mb-4">
-              <div className="flex items-center gap-2">
-                <div className="flex items-center gap-2">
-                  <span className="text-sm font-medium bg-amber-300 px-[10px] py-1 rounded-full">
-                    {t.id}
-                  </span>
-                  <label htmlFor="text" className="text-teal-500 capitalize">
-                    text input
-                  </label>
-                  <input
-                    type="text"
-                    value={t.text}
-                    onChange={(e) => updateText(t.id, { text: e.target.value })}
-                    className="px-2 py-1 border border-gray-300 rounded text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    placeholder="Enter text"
-                  />
-                </div>
-                <div className="flex items-center gap-2">
-                  <label
-                    htmlFor="fontSize"
-                    className="text-teal-500 capitalize"
-                  >
-                    font size
-                  </label>
-                  <input
-                    type="range"
-                    min="10"
-                    max="100"
-                    value={t.size}
-                    id="size"
-                    onChange={(e) => updateText(t.id, { size: parseInt(e.target.value) })}
-                  />
-                  <span>{t.size}px</span>
-                </div>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="flex items-center gap-2">
-                  <label htmlFor="color" className="text-teal-500 capitalize">
-                    text color
-                  </label>
-                  <input
-                    type="color"
-                    value={t.color}
-                    className="w-10 h-8 border border-gray-300 rounded"
-                    id="color"
-                    onChange={(e) =>
-                      updateText(t.id, { color: e.target.value })
-                    }
-                  />
-                </div>
-                <div className="flex items-center gap-2">
-                  <label
-                    htmlFor="rotation"
-                    className="text-teal-500 capitalize"
-                  >
-                    rotation
-                  </label>
-                  <input
-                    type="range"
-                    min="0"
-                    max="360"
-                    value={t.rotation}
-                    onChange={(e) =>
-                      updateText(t.id, { rotation: parseInt(e.target.value) })
-                    }
-                  />
-                  <span>{t.rotation}°</span>
+            <div key={t.id} className="flex gap-2 items-start flex-col">
+              <div className="flex items-baseline gap-2 bg-gray-100 p-2 rounded-lg shadow-sm mb-4">
+                <span className="text-sm font-medium bg-amber-300 px-[10px] py-1 rounded-full">
+                  {t.id}
+                </span>
+                <div className="flex flex-col items-start gap-2 w-full">
+                  <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-2">
+                      <label
+                        htmlFor="text"
+                        className="text-teal-500 capitalize"
+                      >
+                        text input
+                      </label>
+                      <input
+                        type="text"
+                        value={t.text}
+                        onChange={(e) =>
+                          updateText(t.id, { text: e.target.value })
+                        }
+                        className="px-2 py-1 border border-gray-300 rounded text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="Enter text"
+                      />
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <label
+                        htmlFor="fontSize"
+                        className="text-teal-500 capitalize"
+                      >
+                        font size
+                      </label>
+                      <input
+                        type="range"
+                        min="10"
+                        max="100"
+                        value={t.size}
+                        id="size"
+                        onChange={(e) =>
+                          updateText(t.id, { size: parseInt(e.target.value) })
+                        }
+                      />
+                      <span>{t.size}px</span>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2 justify-between w-full">
+                    <div className="flex items-center gap-2">
+                      <label
+                        htmlFor="color"
+                        className="text-teal-500 capitalize"
+                      >
+                        text color
+                      </label>
+                      <input
+                        type="color"
+                        value={t.color}
+                        className="w-10 h-8 border border-gray-300 rounded"
+                        id="color"
+                        onChange={(e) =>
+                          updateText(t.id, { color: e.target.value })
+                        }
+                      />
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <label
+                        htmlFor="rotation"
+                        className="text-teal-500 capitalize"
+                      >
+                        rotation
+                      </label>
+                      <input
+                        type="range"
+                        min="0"
+                        max="360"
+                        value={t.rotation}
+                        onChange={(e) =>
+                          updateText(t.id, {
+                            rotation: parseInt(e.target.value),
+                          })
+                        }
+                      />
+                      <span>{t.rotation}°</span>
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => removeText(t.id)}
+                        className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-3 rounded-full"
+                      >
+                        X
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
